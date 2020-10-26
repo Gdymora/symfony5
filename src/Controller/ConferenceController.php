f1e9a77d44fc992959f633027f4516ec9c666aac
Alexandr <alekspetrofint@gmail.com>
2020-10-26 17:12:14 +0200

    Creating Routes as Annotations

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/conference", name="conference")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
